658ac2ba05d1a4f673898565bf6270a12679357c
Jafar610 <jafarmohamed955@gmail.com>
2026-03-25 15:40:14 +0300

    file updated

--- js.js
+++ js.js
@@ -9,16 +9,22 @@
 // userData();
 
 
-async function cats(){
-    try {
-        const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME');
-        const data = await res.json();
-        console.log(data)
-        const value = data.map(data => `<img src = '${data.url}'/>`)
-        document.getElementById('container').innerHTML = `${value}`
-    } catch (error) {
-        console.log("Error:", error);
-    }
-}
+// async function cats(){
+//     try {
+//         const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME');
+//         const data = await res.json();
+//         console.log(data)
+//         const value = data.map(data => `<img src = '${data.url}'/>`)
+//         document.getElementById('container').innerHTML = `${value}`
+//     } catch (error) {
+//         console.log("Error:", error);
+//     }
+// }
 
-cats();
+// cats();
+
+
+axios.get('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME').then(res=>{
+   const value = res.data.map(data=>`<img src ='${data.url}'/>`);
+    document.getElementById('container').innerHTML = `${value}`
+})
