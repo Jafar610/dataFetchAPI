57d5af7eecb559088f5399f25991393411d3c2a9
Jafar610 <jafarmohamed955@gmail.com>
2026-03-25 14:35:34 +0300

    file updated

--- js.js
+++ js.js
@@ -1,9 +1,24 @@
-async function userData(){
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
-    const htmlValue = data.map(data => `<li>${data.email}</li>`).join("");
-    document.getElementById('container').innerHTML = `${htmlValue}`
-    console.log(data);
+// async function userData(){
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const data = await res.json();
+//     const htmlValue = data.map(data => `<li>${data.email}</li>`).join("");
+//     document.getElementById('container').innerHTML = `${htmlValue}`
+//     console.log(data);
+// }
+
+// userData();
+
+
+async function cats(){
+    try {
+        const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME');
+        const data = await res.json();
+        console.log(data)
+        const value = data.map(data => `<img src = '${data.url}'/>`)
+        document.getElementById('container').innerHTML = `${value}`
+    } catch (error) {
+        console.log("Error:", error);
+    }
 }
 
-userData();
+cats();
